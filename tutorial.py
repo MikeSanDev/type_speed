93c4da07fbc030f68cf2902b63493db599eb4832
MikeSanDev <mjs070594@gmail.com>
2023-10-29 13:40:30 -0500

    added text after initial screen

--- tutorial.py
+++ tutorial.py
@@ -13,6 +13,16 @@ def start_screen(stdscr):
     stdscr.getkey()
 
 
+def wpm_test(stdscr):
+    target_text = 'Hello world this is a test'  # first string that gets shown
+    current_text = []
+
+    stdscr.clear()  # clears
+    stdscr.addstr(target_text)  # adds string
+    stdscr.refresh()  # refreshes screen# program does'nt immediately close, it needs an interaction from the user
+    stdscr.getkey()
+
+
 def main(stdscr):
     # changes text color and then, bg  color
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -20,6 +30,8 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     # function is called to show Press any key text
     start_screen(stdscr)
+    wpm_test(stdscr)
+
 
     # wrapper is a function, will print out hello world
 wrapper(main)
